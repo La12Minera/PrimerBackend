557ce706d330771e6169701723d158c2cb214b44
FedericoAbeldaño <a.fede73@hotmail.com>
2022-04-08 20:14:29 -0300

    4 commit

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const server = http.Server(app);
 expressConfig(app);
 connectSocket(server);
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3001;
 
 // Start server
 server.listen(PORT, () => {
