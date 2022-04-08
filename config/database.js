c3baa1f658e5d0b85abba843f9616fd67596ea44
FedericoAbeldaño <a.fede73@hotmail.com>
2022-04-08 09:51:06 -0300

    1 commit

--- config/database.js
+++ config/database.js
@@ -2,14 +2,20 @@ const mongoose = require('mongoose');
 
 const { log } = require('../utils/logger');
 
-const URI = process.env.DB_URI;
+const server =
+  process.env.MONGO_DB_SERVER ||
+  'Rogerfederer1234:Rogerfeder1234@cursos.u3uyo.mongodb.net';
+const database = process.env.MONGO_DB_DATABASE || 'agenda';
 
 async function connectDB() {
   try {
-    await mongoose.connect(URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(
+      `mongodb+srv://${server}/${database}?retryWrites=true&w=majority`,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      },
+    );
     log.info('MongoDB Connected');
   } catch (error) {
     log.error(error);
